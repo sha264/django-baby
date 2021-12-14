1ab5b37fc6ff75a83154529bf0a387622904381d
稗田直也 <naoyahieda@gmail.com>
2021-12-14 19:16:23 +0900

    deleted explanation

--- config/settings.py
+++ config/settings.py
@@ -91,9 +91,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
