7bdddba337ef77bbdf127e7937078482a76721b3
Hibagon1go <hibarayoichiro871@gmail.com>
2022-05-08 18:00:18 +0900

    Django Baby超入門その4

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path('sample',views.blog_sample),
+    path("sample", views.blog_sample),
+    path("render_html", views.render_html),
+    path("render_html_with_variables", views.render_html_with_variables),
+    path("render_html_with_if_sentences", views.render_html_with_if_sentenses),
+    path("render_html_with_for_sentences", views.render_html_with_for_sentenses),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def blog_sample(request):
-    return HttpResponse('blogアプリの作成完了！')
+    return HttpResponse("blogアプリの作成完了！")
+
+
+def render_html(request):
+    return render(request, "blog/index.html", {})
+
+
+def render_html_with_variables(request):
+    return render(request, "blog/use_variables.html", {"food": "餃子"})
+
+
+def render_html_with_if_sentenses(request):
+    return render(request, "blog/use_if_sentences.html", {"food": "回鍋肉"})
+
+
+def render_html_with_for_sentenses(request):
+    return render(request, "blog/use_for_sentences.html", {"foods": ["餃子", "回鍋肉"]})
